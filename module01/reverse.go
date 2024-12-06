ad277b0be072c74e3ca544424544d2979e233836
Ilya Vayngolts <ilyavayngolts@gmail.com>
2024-12-06 22:43:24 +0200

    feat(module01): implement Reverse func

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,17 @@
 package module01
 
+import "strings"
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
-//   Reverse("cat") => "tac"
-//   Reverse("alphabet") => "tebahpla"
-//
+//	Reverse("cat") => "tac"
+//	Reverse("alphabet") => "tebahpla"
 func Reverse(word string) string {
-	return ""
+	var reversedStringBuilder strings.Builder
+	for i := len(word) - 1; i >= 0; i-- {
+		reversedStringBuilder.WriteByte(word[i])
+	}
+
+	return reversedStringBuilder.String()
 }
