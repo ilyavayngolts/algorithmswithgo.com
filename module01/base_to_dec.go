b0f4e4630cd97e0d0e8b5b49547edcfefabe4945
Ilya Vayngolts <ilyavayngolts@gmail.com>
2024-12-07 10:55:18 +0200

    feat(module01): implement BaseToDec func

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,13 +1,38 @@
 package module01
 
+import "math"
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
 // Eg:
 //
-//   BaseToDec("E", 16) => 14
-//   BaseToDec("1110", 2) => 14
-//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
 func BaseToDec(value string, base int) int {
-	return 0
+	res := 0
+	numbers := map[rune]int{
+		'0': 0,
+		'1': 1,
+		'2': 2,
+		'3': 3,
+		'4': 4,
+		'5': 5,
+		'6': 6,
+		'7': 7,
+		'8': 8,
+		'9': 9,
+		'A': 10,
+		'B': 11,
+		'C': 12,
+		'D': 13,
+		'E': 14,
+		'F': 15,
+	}
+
+	for i, v := range value {
+		res += numbers[v] * int(math.Pow(float64(base), float64(len(value)-1-i)))
+	}
+
+	return res
 }
