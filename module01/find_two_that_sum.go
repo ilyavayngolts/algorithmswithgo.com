7ec4498466662a493e8f69b3f97987f48763ff1c
Ilya Vayngolts <ilyavayngolts@gmail.com>
2024-12-08 20:40:13 +0200

    feat(module01): implement FindTwoThatSum func

--- module01/find_two_that_sum.go
+++ module01/find_two_that_sum.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"maps"
+	"slices"
+)
+
 // FindTwoThatSum will look for two numbers in the provided
 // numbers slice that sum up to the sum argument. It will then
 // return the indices of each of these numbers.
@@ -9,16 +14,16 @@ package module01
 //
 // Eg:
 //
-//   FindTwoThatSum([1,2,3,4], 7) => (2, 3)
-//   FindTwoThatSum([0, -1, 1], 0) => (1, 2)
-//   FindTwoThatSum([0, 1, 1], 0) => (-1, -1)
+//	FindTwoThatSum([1,2,3,4], 7) => (2, 3)
+//	FindTwoThatSum([0, -1, 1], 0) => (1, 2)
+//	FindTwoThatSum([0, 1, 1], 0) => (-1, -1)
 //
 // Or if we have duplicate answers any of them are okay. All
 // of the following are correct.
 //
-//   FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (5, 6)
-//   FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 3)
-//   FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 7)
+//	FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (5, 6)
+//	FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 3)
+//	FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 7)
 //
 // Each number will only be used once, and the original numbers
 // list should not be rearranged or altered in any way.
@@ -27,13 +32,26 @@ package module01
 // yourself further, try adding different criteria for which
 // numbers will be used when there are multiple correct answers.
 // Eg:
-// 1. Always return the lowest index possible for the first value.
-// 2. Always return the index of lowest value possible for the
-//    first return value.
-// 3. Always return the index of the two values who have a minimal
-//    difference. Eg prefer the values 2, 2 over 1, 3 over 0, 4
-//    for the sum of 4.
-//
+//  1. Always return the lowest index possible for the first value.
+//  2. Always return the index of lowest value possible for the
+//     first return value.
+//  3. Always return the index of the two values who have a minimal
+//     difference. Eg prefer the values 2, 2 over 1, 3 over 0, 4
+//     for the sum of 4.
 func FindTwoThatSum(numbers []int, sum int) (int, int) {
-	return 0, 0
+	indices := [2]int{-1, -1}
+	possiblePairs := make(map[int]int)
+
+	for i, num := range numbers {
+		values := slices.Collect(maps.Keys(possiblePairs))
+		pair := sum - num
+		if slices.Contains(values, pair) {
+			indices[0] = i
+			indices[1] = possiblePairs[pair]
+		} else {
+			possiblePairs[num] = i
+		}
+	}
+
+	return indices[0], indices[1]
 }
