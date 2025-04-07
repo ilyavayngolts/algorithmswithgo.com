4eff09d13699a9dd3e7237549d1e3a7b3daba81b
Ilya Vayngolts <ilyavayngolts@gmail.com>
2025-04-07 21:19:53 +0300

    feat(module01): implement Fibonacci func

--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -4,26 +4,37 @@ package module01
 //
 // A Fibonacci number N is defined as:
 //
-//   Fibonacci(N) = Fibonacci(N-1) + Fibonacci(N-2)
+//	Fibonacci(N) = Fibonacci(N-1) + Fibonacci(N-2)
 //
 // Where the following base cases are used:
 //
-//   Fibonacci(0) => 0
-//   Fibonacci(1) => 1
-//
+//	Fibonacci(0) => 0
+//	Fibonacci(1) => 1
 //
 // Examples:
 //
-//   Fibonacci(0) => 0
-//   Fibonacci(1) => 1
-//   Fibonacci(2) => 1
-//   Fibonacci(3) => 2
-//   Fibonacci(4) => 3
-//   Fibonacci(5) => 5
-//   Fibonacci(6) => 8
-//   Fibonacci(7) => 13
-//   Fibonacci(14) => 377
-//
+//	Fibonacci(0) => 0
+//	Fibonacci(1) => 1
+//	Fibonacci(2) => 1
+//	Fibonacci(3) => 2
+//	Fibonacci(4) => 3
+//	Fibonacci(5) => 5
+//	Fibonacci(6) => 8
+//	Fibonacci(7) => 13
+//	Fibonacci(14) => 377
 func Fibonacci(n int) int {
-	return 0
+	if n <= 1 {
+		return n
+	}
+
+	nMinus2 := 0
+	nMinus1 := 1
+	current := 0
+	for i := 2; i <= n; i++ {
+		current = nMinus2 + nMinus1
+		nMinus2 = nMinus1
+		nMinus1 = current
+	}
+
+	return current
 }
