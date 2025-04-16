de6564d19abb07200dd18aa4ceb5c4f94bf65fa3
Ilya Vayngolts <ilyavayngolts@gmail.com>
2025-04-16 13:24:33 +0300

    feat(module02): implement bubble sort

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -6,19 +6,41 @@ import "sort"
 //
 // Big O: O(N^2), where N is the size of the list
 func BubbleSortInt(list []int) {
+	for i := 0; i < len(list); i++ {
+		for j := 0; j < len(list)-1-i; j++ {
+			if list[j] > list[j+1] {
+				list[j], list[j+1] = list[j+1], list[j]
+			}
+		}
+	}
 }
 
 // BubbleSortString is a bubble sort for string slices. Try implementing it for
 // practice.
 func BubbleSortString(list []string) {
+	for i := 0; i < len(list); i++ {
+		for j := 0; j < len(list)-1-i; j++ {
+			if list[j] > list[j+1] {
+				list[j], list[j+1] = list[j+1], list[j]
+			}
+		}
+	}
 }
 
 // BubbleSortPerson uses bubble sort to sort Person slices by: Age, then
 // LastName, then FirstName. Try implementing it for practice.
-func BubbleSortPerson(people []Person) {
+func BubbleSortPerson(people People) {
+	BubbleSort(people)
 }
 
 // BubbleSort uses the standard library's sort.Interface to sort. Try
 // implementing it for practice.
 func BubbleSort(list sort.Interface) {
+	for i := 0; i < list.Len(); i++ {
+		for j := 0; j < list.Len()-1-i; j++ {
+			if list.Less(j+1, j) {
+				list.Swap(j+1, j)
+			}
+		}
+	}
 }
--- module02/person.go
+++ module02/person.go
@@ -6,3 +6,25 @@ type Person struct {
 	FirstName string
 	LastName  string
 }
+
+type People []Person
+
+func (p People) Len() int {
+	return len(p)
+}
+
+func (p People) Less(i, j int) bool {
+	if p[i].Age != p[j].Age {
+		return p[i].Age < p[j].Age
+	}
+
+	if p[i].LastName != p[j].LastName {
+		return p[i].LastName < p[j].LastName
+	}
+
+	return p[i].FirstName < p[j].FirstName
+}
+
+func (p People) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
